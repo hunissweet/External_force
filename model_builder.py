787b95823a149401ea765ce5708684e46cd3738f
hunissweet <66056771+hunissweet@users.noreply.github.com>
2023-10-16 11:10:45 +0200

    Add files via upload

---
+++ model_builder.py
@@ -0,0 +1,85 @@
+import torch
+from torch import nn 
+class LSTMModel(nn.Module):
+    def __init__(self, input_dim : int, hidden_dim:int, layer_dim:int, output_dim:int, dropout_prob:float):
+        super(LSTMModel, self).__init__()
+
+        # Defining the number of layers and the nodes in each layer
+        self.hidden_dim = hidden_dim
+        self.layer_dim = layer_dim
+
+        # LSTM layers
+        self.lstm = nn.LSTM(
+            input_dim, hidden_dim, layer_dim, batch_first=True, dropout=dropout_prob
+        )
+
+        # Fully connected layer
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        # Initializing hidden state for first input with zeros
+        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
+
+        # Initializing cell state for first input with zeros
+        c0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
+
+        # We need to detach as we are doing truncated backpropagation through time (BPTT)
+        # If we don't, we'll backprop all the way to the start even after going through another batch
+        # Forward propagation by passing in the input, hidden state, and cell state into the model
+        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+
+        # Reshaping the outputs in the shape of (batch_size, seq_length, hidden_size)
+        # so that it can fit into the fully connected layer
+        out = out[:, -1, :]
+
+        # Convert the final state to our desired output shape (batch_size, output_dim)
+        out = self.fc(out)
+
+        return out
+
+class TinyVGG(nn.Module):
+    '''  
+        Args:
+        input_shape: An integer indicating number of input channels.
+        hidden_units: An integer indicating number of hidden units between layers.
+        output_shape: An integer indicating number of output units.
+    '''
+
+    def __init__(self, input_dim: int, hidden_dim: int, output_shape: int) -> None:
+        super().__init__()
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape, 
+                    out_channels=hidden_units, 
+                    kernel_size=3, 
+                    stride=1, 
+                    padding=0),  
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units, 
+                    out_channels=hidden_units,
+                    kernel_size=3,
+                    stride=1,
+                    padding=0),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2,
+                        stride=2))
+
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=0),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+              )
+        self.classifier = nn.Sequential(
+              nn.Flatten(),
+              # Where did this in_features shape come from? 
+              # It's because each layer of our network compresses and changes the shape of our inputs data.
+              nn.Linear(in_features=hidden_units*13*13,
+                        out_features=output_shape)
+              )
+
+    def forward(self, x: torch.Tensor):
+        x = self.conv_block_1(x)
+        x = self.conv_block_2(x)
+        x = self.classifier(x)
+        return x
